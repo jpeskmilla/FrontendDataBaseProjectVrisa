e61035f16a78ed54ef8eb24652cf3e2a25373027
Juan Pablo Escamilla <juanpabloeskmilla@gmail.com>
2025-11-22 15:11:15 -0500

    refactor: redesign homepage layout and styles; implement responsive design and update component structure

--- src/app/pages/HomePage.jsx
+++ src/app/pages/HomePage.jsx
@@ -4,11 +4,38 @@ import React from "react";
 import Logo from "../../assets/vrisa_logo.png";
 import homepage from "../../assets/homepage.jpg";
 import "./homepage-styles.css";
+import { LoginInput } from "../../shared/components/Input";
+
 export default function Desktop1() {
   return (
-    <div>
-      <img src={homepage} alt="homepage" className="desktop1-homepage" />
-      <img src={Logo} alt="Logo" className="desktop1-logo" />
+    <div className="landing-wrapper">
+      <div className="image-container">
+        <img src={homepage} alt="homepage" className="homepage-image" />
+      </div>
+
+      <div className="homepage-container">
+        <div className="homepage-welcome-container">
+          <strong>¡Bienvenido!</strong>
+          <p>Ingresa tu correo y contraseña para iniciar</p>
+        </div>
+
+        <div className="form-container">
+          <LoginInput label="*  Correo" className="form-input" />
+          <LoginInput label="* Contraseña" className="form-input" />
+        </div>
+
+        <p className="forget-text">¿Olvidaste tu contraseña?</p>
+
+        <button type="button" className="login-button">
+          Iniciar sesión
+        </button>
+
+        <p className="register-text">
+          ¿No tienes cuenta? <span>Regístrate</span>
+        </p>
+
+        <img src={Logo} alt="Logo" className="vrisa-logo" />
+      </div>
     </div>
   );
 }
